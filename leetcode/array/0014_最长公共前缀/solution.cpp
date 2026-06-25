eac0eabf9d14d08a07b370efc4c08b2f2ea64c71
Dtsyyds <1640028011@qq.com>
2026-06-25 10:33:59 +0800

    0014_最长公共前缀方法及题解

---
+++ leetcode/array/0014_最长公共前缀/solution.cpp
@@ -0,0 +1,57 @@
+# include <vector>
+# include <iostream>
+# include <algorithm>
+# include <string>
+
+using namespace std;
+
+class Solution_Base {
+    public:
+        string longestCommonPrefix(vector<string>& strs)
+        {
+            if (strs.empty()) return "";
+            string prefix = strs[0];
+            for(int i = 0; i < strs.size(); i++)
+            {
+                prefix = longestCommonPrefix(prefix, strs[i]);
+            }
+            return prefix;
+
+        }
+
+        string longestCommonPrefix(const string& strs1, const string& strs2)
+        {
+            int length = min(strs1.size(), strs2.size());
+
+            for (int i = 0; i < length; i++)
+            {
+                if (strs1[i] != strs2[i])
+                    return strs1.substr(0, i);
+            }
+            return "";
+        }
+        
+};
+
+class Solution_Pro {
+    public:
+        string longestCommonPrefix(vector<string>& strs)
+        {
+            if (strs.empty()) return "";
+            sort(strs.begin(), strs.end());
+            string prefix = strs[0];
+
+            for (int i = 0; i < strs.size(); i++)
+            {
+                for(int j = 0; j < prefix.size(); j++)
+                {
+                    if(prefix[j] != strs[i][j])
+                    {
+                        prefix = prefix.substr(0, j);
+                        break;
+                    }
+                }
+            }
+            return prefix;
+        }
+}
